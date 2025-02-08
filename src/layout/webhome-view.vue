f5af69ec73ae41ed98de2039bde98c1cf9e6169b
qiuye-zhou <zhouzw1302@163.com>
2025-02-08 15:45:27 +0800

    fix: website dark light

--- src/layout/webhome-view.vue
+++ src/layout/webhome-view.vue
@@ -17,9 +17,23 @@ onMounted(() => {
 </script>
 
 <template>
-    <div>
+    <div class="transitions">
         <WebHeader/>
-        <router-view />
+        <router-view v-slot="{ Component }">
+            <Suspense>
+                <component :is="Component" />
+            </Suspense>
+        </router-view>
         <WebFooter/>
     </div>
 </template>
+<style scoped>
+.transitions {
+    transition: left 0.15s;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+}
+:is([data-theme="dark"] .transitions) {
+    background-color: transparent;
+    color: aliceblue;
+}
+</style>
