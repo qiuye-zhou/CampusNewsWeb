50ae3330dfd28014f1f0b39d8ee2d41808ac9650
qiuye-zhou <zhouzw1302@163.com>
2025-02-09 16:00:24 +0800

    update: website footer add theme

--- src/components/website/WebFootercom/RightInfo.vue
+++ src/components/website/WebFootercom/RightInfo.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
+import { useStoreRef as useStoreReference } from '~/hooks/useStoreRef'
+import { UIStore } from '~/stores/ui'
+
+import { Dark, Light } from '~/components/icons/index'
+
+const { isDark, toggleDark } = useStoreReference(UIStore)
 </script>
 
 <template>
     <div class="mt-12 space-y-3 text-center md:mt-6 md:text-left">
       <p>
-        <span>GitHub：</span>
-        <a href="https://github.com/qiuye-zhou" target="_blank">
-            qiuye
-        </a>
-      </p>
-      <p>
         <span>邮箱：zhouzw1302@163.com</span>
+      </p>
+      <p class=" flex">
+        <p>主题：</p>
+        <button class="flex justify-center" @click="() => void toggleDark()">
+            <div class="m-auto">
+              <light v-if="!isDark" />
+              <dark v-else />
+            </div>
+        </button>
       </p>
     </div>
 </template>
