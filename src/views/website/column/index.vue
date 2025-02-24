76bd5757b0183ba8ccee514e2be3bbd581f71834
qiuye-zhou <zhouzw1302@163.com>
2025-02-24 18:56:51 +0800

    Update index.vue

--- src/views/website/column/index.vue
+++ src/views/website/column/index.vue
@@ -15,10 +15,14 @@ function handleRoute(item: string, tag: string) {
 <template>
     <div
         class="overflow-auto w-full relative h-[100vh]
-        flex items-center justify-center"
+        flex items-center justify-center flex-wrap"
     >
-        <h1>column</h1>
-        <hr/>
-        <button class="bg-slate-300" @click="handleRoute('/column/tag', 'test')">tets tag button: tagtest</button>
+        <div size="large" class="cursor-pointer border-4 p-12 mx-12 my-0"  @click="handleRoute('/column/tag', 'test1')">学科信息: test1</div>
+        <div size="large" class="cursor-pointer border-4 p-12 mx-12 my-0"  @click="handleRoute('/column/tag', 'test2')">学科信息: test2</div>
+        <div size="large" class="cursor-pointer border-4 p-12 mx-12 my-0"  @click="handleRoute('/column/tag', 'test3')">学科信息: test3</div>
+        <div size="large" class="cursor-pointer border-4 p-12 mx-12 my-0"  @click="handleRoute('/column/tag', 'test')">学科信息: test</div>
+        <div size="large" class="cursor-pointer border-4 p-12 mx-12 my-0"  @click="handleRoute('/column/tag', 'test')">学科信息: test</div>
+        <div size="large" class="cursor-pointer border-4 p-12 mx-12 my-0"  @click="handleRoute('/column/tag', 'test')">学科信息: test</div>
+        <div size="large" class="cursor-pointer border-4 p-12 mx-12 my-0"  @click="handleRoute('/column/tag', 'test')">学科信息: test</div>
     </div>
 </template>
