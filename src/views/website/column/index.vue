e8777127f8210406091fc3345c2cccaf9613594f
qiuye-zhou <zhouzw1302@163.com>
2025-02-27 16:54:43 +0800

    Update index.vue

--- src/views/website/column/index.vue
+++ src/views/website/column/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import { NCard } from 'naive-ui'
+
+import NewCard from '~/components/website/NewCard/index.vue'
 
 const router = useRouter()
 function handleRoute(item: string, tag: string) {
@@ -14,6 +17,37 @@ function handleRoute(item: string, tag: string) {
 
 <template>
     <div
+        class="w-full relative
+        flex items-center justify-center flex-wrap"
+    >
+        <Carousel/>
+        <n-card title="专栏1" size="huge" header-style="border-width: 2px;" content-style="border-width: 2px;">
+            <template #header-extra>
+                <p style="cursor: pointer;"  @click="handleRoute('/column/tag', '11111')">更多</p>
+            </template>
+            <NewCard/>
+            <NewCard/>
+            <NewCard/>
+        </n-card>
+        <n-card title="专栏2" size="huge" header-style="border-width: 2px;" content-style="border-width: 2px;">
+            <template #header-extra>
+                <p style="cursor: pointer;"  @click="handleRoute('/column/tag', '22222')">更多</p>
+            </template>
+            <NewCard/>
+        </n-card>
+        <n-card title="专栏3" size="huge" header-style="border-width: 2px;" content-style="border-width: 2px;">
+            <template #header-extra>
+                <p style="cursor: pointer;"  @click="handleRoute('/column/tag', '33333')">更多</p>
+            </template>
+            <NewCard/>
+            <NewCard/>
+            <NewCard/>
+        </n-card>
+    </div>
+</template>
+
+<!-- <template>
+    <div
         class="overflow-auto w-full relative h-[100vh]
         flex items-center justify-center flex-wrap"
     >
@@ -25,4 +59,4 @@ function handleRoute(item: string, tag: string) {
         <div size="large" class="cursor-pointer border-4 p-12 mx-12 my-0"  @click="handleRoute('/column/tag', 'test')">学科信息: test</div>
         <div size="large" class="cursor-pointer border-4 p-12 mx-12 my-0"  @click="handleRoute('/column/tag', 'test')">学科信息: test</div>
     </div>
-</template>
+</template> -->
