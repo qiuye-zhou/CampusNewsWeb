473a941b7791777625505b2bbdc885c6b1f82d09
qiuye-zhou <zhouzw1302@163.com>
2025-03-13 16:38:22 +0800

    Update index.vue

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,26 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useStoreRef as useStoreReference } from '~/hooks/useStoreRef'
-import { useUserStore } from '~/stores/user'
+import { onMounted, reactive } from 'vue'
 import { parseDate } from '~/utils/time'
+import storage from '~/utils/localstorage'
+import { UserDate } from '~/utils/token'
+const DataList = reactive({
+    lastLoginIp: '',
+    lastLoginTime: '',
+    name: '',
+    grade: '',
+    updateTime: new Date()
+})
 
-const userStore = useStoreReference(useUserStore)
-const statTime = ref(null as unknown as Date)
+onMounted(() => {
+    const userData = storage.get(UserDate)
+    DataList.lastLoginIp = userData.lastLoginIp
+    DataList.lastLoginTime = userData.lastLoginTime
+    DataList.name = userData.name
+    DataList.grade = userData.grade ? '管理员' : '编辑'
+    DataList.updateTime = new Date()
+})
 </script>
 
 <template>
     <div>
         <h1 class="text-2xl font-light my-9">
-            欢迎回来
+            欢迎回来{{ DataList.grade }}{{ DataList.name }}
         </h1>
         <h1 class="text-base my-3">
             登录记录
         </h1>
         <p class="text-xs flex flex-col my-3">
-            <span class="my-1">上次登录 IP：{{ userStore.user.value?.lastLoginIp ?? 'N/A' }}</span>
+            <span class="my-1">上次登录 IP：{{ DataList.lastLoginIp ?? 'N/A' }}</span>
             <span class="my-1">
-                上次登录时间：{{ userStore.user.value?.lastLoginTime ?
-                    parseDate(userStore.user.value?.lastLoginTime, 'yyyy 年 M 月 d 日 HH:mm:ss',)
+                上次登录时间：{{ DataList.lastLoginTime ?
+                    parseDate(DataList.lastLoginTime, 'yyyy 年 M 月 d 日 HH:mm:ss',)
                     : 'N/A' }}
             </span>
         </p>
@@ -28,8 +41,8 @@ const statTime = ref(null as unknown as Date)
             数据统计
         </h1>
         <p class="text-xs flex flex-col my-3">
-            <span class="my-1">数据更新于：{{ statTime
-                ? parseDate(statTime, 'yyyy 年 M 月 d 日 HH:mm:ss')
+            <span class="my-1">数据更新于：{{ DataList.updateTime
+                ? parseDate(DataList.updateTime, 'yyyy 年 M 月 d 日 HH:mm:ss')
                 : 'N/A' }}</span>
         </p>
     </div>
