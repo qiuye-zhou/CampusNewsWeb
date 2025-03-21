b7add3081f12a3e57a68b8177e288b1f28f423fb
qiuye-zhou <zhouzw1302@163.com>
2025-03-21 16:29:59 +0800

    Update index.vue

--- src/views/website/detail/index.vue
+++ src/views/website/detail/index.vue
@@ -20,14 +20,12 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div
-        class="overflow-auto w-full relative h-[100vh]
-        flex items-center flex-col"
-    >
+    <div class="min-h-screen flex items-center flex-col">
         <h1 class="mt-10 text-2xl">{{ DataList.new.title }}</h1>
         <p class="text-xs my-2">{{ parseDate(DataList.new.created, 'yyyy 年 M 月 d 日 HH:mm:ss') }}</p>
         <p class="text-xs my-2">作者：{{ DataList.new.username }}</p>
         <p class="text-xs my-2">浏览次数：{{ DataList.new.browsenum }}</p>
         <p class="text-lg my-2">{{ DataList.new.detail ?? '暂无介绍' }}</p>
+        <pre class="p-12 whitespace-pre-wrap break-words">{{ DataList.new.text }}</pre>
     </div>
 </template>
